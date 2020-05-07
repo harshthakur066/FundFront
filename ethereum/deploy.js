dea32443ce5d61ae88273f1fb85263c28efa8c68
harshthakur066 <harshthakur066@gmail.com>
2020-05-07 20:12:55 +0530

    added deplyed script

---
+++ ethereum/deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+  "virtual identify peasant boss dress toss mix buzz panic private scrub south",
+  "https://rinkeby.infura.io/v3/53187f1aefc74058b52ecd2ccba8f20b"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+
+deploy();
