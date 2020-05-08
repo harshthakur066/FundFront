77752d1fbd32cf8e239b12033c9e9d63aadb647a
harshthakur066 <harshthakur066@gmail.com>
2020-05-09 00:46:04 +0530

    updated in get initial props

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from "react";
 import factory from "../ethereum/factory";
 
-function CampaignIndex() {
-  const getCampaignsList = async () => {
-    const campaigns = await factory.methods.getDeployedCampaigns().call();
+const CampaignIndex = ({ campaigns }) => {
+  return (
+    <div>
+      <div>Campaigns:</div>
+      <div>{campaigns[0]}</div>
+    </div>
+  );
+};
 
-    console.log(campaigns);
-  };
-  useEffect(() => {
-    getCampaignsList();
-  }, []);
-  return <div>Campaign</div>;
-}
+CampaignIndex.getInitialProps = async () => {
+  const campaigns = await factory.methods.getDeployedCampaigns().call();
+  return { campaigns };
+};
 
 export default CampaignIndex;
