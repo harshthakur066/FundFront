750985363cd2c33fc63306d978c8e41ddeca48bd
harshthakur066 <harshthakur066@gmail.com>
2020-05-10 19:54:29 +0530

    added css and campaign list page

---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+export default () => {
+  return (
+    <Menu style={{ marginTop: 10 }}>
+      <Menu.Item>
+        <b>FundFront</b>
+      </Menu.Item>
+      <Menu.Menu position="right">
+        <Menu.Item>Campaigns</Menu.Item>
+        <Menu.Item>+</Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,28 @@
-import React, { useEffect } from "react";
+import React from "react";
 import factory from "../ethereum/factory";
+import { Card, Button } from "semantic-ui-react";
+import Layout from "../components/Layout";
 
 const CampaignIndex = ({ campaigns }) => {
+  const renderCampaigns = () => {
+    const items = campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+    return <Card.Group items={items} />;
+  };
+
   return (
-    <div>
-      <div>Campaigns:</div>
-      <div>{campaigns[0]}</div>
-    </div>
+    <Layout>
+      <h1>Open Campaigns</h1>
+      <div>
+        <Button floated="right" content="Create Campaign" icon="add" primary />
+      </div>
+      <div> {renderCampaigns()} </div>
+    </Layout>
   );
 };
 
