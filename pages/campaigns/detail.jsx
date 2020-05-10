947cd5b171f588407c3e766aa0280922a4eb48ce
harshthakur066 <harshthakur066@gmail.com>
2020-05-11 01:20:00 +0530

    displayed campaign details as cards

--- pages/campaigns/detail.jsx
+++ pages/campaigns/detail.jsx
@@ -1,11 +1,73 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
+import Layout from "../../components/Layout";
+import Campaign from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
 
-const CampaignDetail = () => {
+const CampaignDetail = ({
+  minimumContribution,
+  balance,
+  requestsCount,
+  approversCount,
+  manager,
+}) => {
+  const renderCards = () => {
+    const items = [
+      {
+        header: manager,
+        meta: "Address of Manager",
+        description:
+          "The manager created this campaign and can create requests to withdraw money",
+        style: { overflowWrap: "break-word" },
+      },
+      {
+        header: minimumContribution,
+        meta: "Minimum Contribution (wei)",
+        description:
+          "You must contribute at least this much wei to become an approver",
+      },
+      {
+        header: requestsCount,
+        meta: "Number of Requests",
+        description:
+          "A request tries to withdraw money from the contract. Requests must be approved by approvers",
+      },
+      {
+        header: approversCount,
+        meta: "Number of Approvers",
+        description:
+          "Number of people who have already donated to this campaign",
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance (ether)",
+        description:
+          "The balance is how much money this campaign has left to spend.",
+      },
+    ];
+
+    return <Card.Group items={items} />;
+  };
+
   return (
-    <div>
+    <Layout>
       <h2>Campaign Details</h2>
-    </div>
+      {renderCards()}
+    </Layout>
   );
+};
+
+CampaignDetail.getInitialProps = async ({ query }) => {
+  const campaign = Campaign(query.address);
+  const summary = await campaign.methods.getSummary().call();
+  //   console.log(summary); //returns a result oject with keys in n. 0,1,2...
+  return {
+    minimumContribution: summary[0],
+    balance: summary[1],
+    requestsCount: summary[2],
+    approversCount: summary[3],
+    manager: summary[4],
+  };
 };
 
 export default CampaignDetail;
