3f0bc01b6f5eff3967fcae13ff6d5392bcd43584
harshthakur066 <harshthakur066@gmail.com>
2020-05-11 21:18:44 +0530

    production setup

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const next = require("next");
 const routes = require("./routes");
-const app = next({ dev: process.env.NODE_ENV !== "production" });
+const PORT = process.env.PORT || 3000;
+const app = next({ dev: process.env.NODE_ENV == "production" });
 const handler = routes.getRequestHandler(app);
 const { createServer } = require("http");
 
 app.prepare().then(() => {
-  createServer(handler).listen(3000, (err) => {
+  createServer(handler).listen(PORT, (err) => {
     if (err) throw err;
-    console.log("Running on port 3000, http://localhost:3000");
+    console.log(`Running on ${PORT} `);
   });
 });
